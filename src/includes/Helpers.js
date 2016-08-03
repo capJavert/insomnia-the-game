08e95b7c4c89b3b4b91f50eb1e7908b57049a514
capJavert <ante.baric3@gmail.com>
2016-08-03 12:32:34 +0200

    added Rock, Fiend, FlyingFiend linear generators

--- src/includes/Helpers.js
+++ src/includes/Helpers.js
@@ -5,13 +5,52 @@ import FlyingFiend from 'objects/FlyingFiend';
 
 class Helpers {
 
-	//generates orb in linear interval
+	//generates Orb in linear interval
 	linearOrbGenerator(main, lvlArray, orbNumber, orbStartPosition, orbPositionY, interval) {
 		var orbSpawnPosition = orbStartPosition;
 		var loopLimit = lvlArray.length+orbNumber;
 
 		for (var i = lvlArray.length; i < loopLimit; i++) {
 			lvlArray.push(new Orb(main.game, orbSpawnPosition, orbPositionY, 1, main.interactionCollision));
+			orbSpawnPosition+=interval;
+		};
+
+		return lvlArray;
+	}
+
+	//generates Rock in linear interval
+	linearRockGenerator(main, lvlArray, orbNumber, orbStartPosition, orbPositionY, interval) {
+		var orbSpawnPosition = orbStartPosition;
+		var loopLimit = lvlArray.length+orbNumber;
+
+		for (var i = lvlArray.length; i < loopLimit; i++) {
+			lvlArray.push(new Rock(main.game, orbSpawnPosition, orbPositionY, 1, main.interactionCollision));
+			orbSpawnPosition+=interval;
+		};
+
+		return lvlArray;
+	}
+
+	//generates Fiend in linear interval
+	linearFiendGenerator(main, lvlArray, orbNumber, orbStartPosition, orbPositionY, interval) {
+		var orbSpawnPosition = orbStartPosition;
+		var loopLimit = lvlArray.length+orbNumber;
+
+		for (var i = lvlArray.length; i < loopLimit; i++) {
+			lvlArray.push(new Fiend(main.game, orbSpawnPosition, orbPositionY, 1, main.interactionCollision));
+			orbSpawnPosition+=interval;
+		};
+
+		return lvlArray;
+	}
+
+	//generates FlyingFiend in linear interval
+	linearFlyingFiendGenerator(main, lvlArray, orbNumber, orbStartPosition, orbPositionY, interval) {
+		var orbSpawnPosition = orbStartPosition;
+		var loopLimit = lvlArray.length+orbNumber;
+
+		for (var i = lvlArray.length; i < loopLimit; i++) {
+			lvlArray.push(new FlyingFiend(main.game, orbSpawnPosition, orbPositionY, 1, main.interactionCollision));
 			orbSpawnPosition+=interval;
 		};
 
