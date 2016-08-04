3e58002e1f4a965cc0b9094b731b05a05a521aa2
capJavert <ante.baric3@gmail.com>
2016-08-04 14:12:07 +0200

    fixed Bitmap class constructor to match Sprite methods

--- src/objects/Bitmap.js
+++ src/objects/Bitmap.js
@@ -3,20 +3,21 @@ import Material from 'objects/Material';
 
 class Bitmap extends Sprite {
 
-	constructor(game, name, x, y, width, height, scale, kinematic){
-		super(game, name, x, y, width, height, scale, kinematic);
+	constructor(game, name, x, y, width, height, scale, collisionGroup, kinematic){
+		super(game, name, x, y, width, height, scale, collisionGroup, kinematic);
 		this.game = game;
-		this.spriteType = name;
+		this.oType = name;
 		this.x = x;
-		this.y = this.game.height-y-height/2;
+		this.y = this.game.height-y;
 		this.width = width;
 		this.height = height;
 		this.scale = scale;
 		this.visible = false;
 		this.kinematic = kinematic;
+		this.collisionGroup = collisionGroup;
 	}
 
-	render(collisionGroup) {
+	render() {
 		//create bitmap data
 		var sprite = this.game.add.bitmapData(this.width, this.height);
 
@@ -25,28 +26,29 @@ class Bitmap extends Sprite {
 	    sprite.ctx.fill();
 
 		this.sprite = this.game.add.sprite(this.x, this.y, sprite);
-
 		this.setScale(this.scale);
 		this.game.physics.p2.enable(this.sprite, this.game.debugMode);
-
+		this.sprite.oType = this.oType; //for check inside collision callback
 	    this.sprite.body.kinematic = this.kinematic;
 	    this.sprite.body.collideWorldBounds = true;
+        this.sprite.body.setCollisionGroup(this.collisionGroup);
 
-        this.sprite.body.setCollisionGroup(collisionGroup);
-
-		this.sprite.position.y -= this.sprite.height;
+		this.sprite.position.y -= (this.sprite.height/2);
 		this.visible = true;
 
 		//set material params
-		this.material = new Material(this.game, this.spriteType, this.sprite.body);
+		this.material = new Material(this.game, this.oType, this.sprite.body);
 	}
 
-	update(speed) {
-		this.sprite.body.setZeroVelocity();
-		this.sprite.body.velocity.y = 10000;
+	update(playerObject) {
+		this.sprite.body.velocity.x = 0;
 
-		if(speed) {
+		if(playerObject.getSpeed()>0) {
 			this.sprite.body.velocity.x = -400;
+		} else if(playerObject.getSpeed()<0) {
+			this.sprite.body.velocity.x = 400;
+		} else {
+			//player is not moving
 		}
 	}
 }
